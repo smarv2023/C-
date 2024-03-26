84e479c9c774588f34ac37106c503848cb2d4a5a
Marvin <153880760+smarv2023@users.noreply.github.com>
2024-03-25 20:00:05 -0500

    Update Arrow and Sword added Weapon Class

--- Excercises/ArrowAndSword/ArrowDamage.cs
+++ Excercises/ArrowAndSword/ArrowDamage.cs
@@ -6,61 +6,20 @@ using System.Threading.Tasks;
 
 namespace ArrowAndSword
 {
-    internal class ArrowDamage
+    internal class ArrowDamage : WeaponDamage
     {
         private const decimal BASE_MULTIPLIER = 0.35M;
         private const decimal MAGIC_MULTIPLIER = 2.5M;
         private const decimal FLAME_DAMAGE = 1.25M;
 
-        public int Damage { get; private set; }
+        public ArrowDamage(int startingRoll) : base(startingRoll) { }
 
-        private int roll;
-        public int Roll
-        {
-            get { return roll; }
-            set
-            {
-                roll = value;
-                CalculateDamage();
-            }
-        }
-
-        private bool magic;
-
-        public bool Magic
-        {
-            get { return magic; }
-            set
-            {
-                magic = value;
-                CalculateDamage();
-            }
-        }
-
-        private bool flaming;
-
-        public bool Flaming
-        {
-            get { return flaming; }
-            set
-            {
-                flaming = value;
-                CalculateDamage();
-            }
-        }
-
-        public void CalculateDamage()
+        protected override void CalculateDamage()
         {
             decimal baseDamage = Roll * BASE_MULTIPLIER;
             if (Magic) baseDamage *= MAGIC_MULTIPLIER;
             if (Flaming) Damage = (int)Math.Ceiling(baseDamage + FLAME_DAMAGE);
             else Damage = (int)Math.Ceiling(baseDamage);
-        }
-
-        public ArrowDamage(int startingRoll)
-        {
-            roll = startingRoll;
-            CalculateDamage();
         }
     }
 }
--- Excercises/ArrowAndSword/SwordDamage.cs
+++ Excercises/ArrowAndSword/SwordDamage.cs
@@ -7,55 +7,14 @@ using System.Diagnostics;
 
 namespace ArrowAndSword
 {
-    internal class SwordDamage
+    internal class SwordDamage : WeaponDamage
     {
-        public SwordDamage(int startingRoll)
-        {
-            roll = startingRoll;
-            CalculateDamage();
-        }
+        private const int BASE_DAMAGE = 3;
+        private const int FLAME_DAMAGE = 2;
+        
+        public SwordDamage(int startingRoll) : base(startingRoll) { }
 
-        public const int BASE_DAMAGE = 3;
-        public const int FLAME_DAMAGE = 2;
-
-        public int Damage { get; private set; }
-
-        private int roll;
-        public int Roll
-        {
-            get { return roll; }
-            set
-            {
-                roll = value;
-                CalculateDamage();
-            }
-        }
-
-        private bool magic;
-
-        public bool Magic
-        {
-            get { return magic; }
-            set
-            {
-                magic = value;
-                CalculateDamage();
-            }
-        }
-
-        private bool flaming;
-
-        public bool Flaming
-        {
-            get { return flaming; }
-            set
-            {
-                flaming = value;
-                CalculateDamage();
-            }
-        }
-
-        public void CalculateDamage()
+        protected override void CalculateDamage()
         {
             decimal magicMultiplier = 1M;
             if (Magic) magicMultiplier = 1.75M;
