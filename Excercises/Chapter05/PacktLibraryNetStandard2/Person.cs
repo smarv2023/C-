c802dc31d7fbe27c725bed73adaeff1a08237ca8
Marvin <153880760+smarv2023@users.noreply.github.com>
2024-05-14 20:22:00 -0500

    Starting new

---
+++ Excercises/Chapter05/PacktLibraryNetStandard2/Person.cs
@@ -0,0 +1,6 @@
+﻿namespace Packt.Shared;
+    public class Person : Object
+    {
+    public string? Name;
+    public DateTime DateOfBirt;
+    }
---
+++ Excercises/Chapter05/PeopleApp/Program.cs
@@ -0,0 +1,6 @@
+﻿using Packt.Shared;
+
+Person bob = new Person();
+bob.Name = "Bob Smith";
+
+WriteLine(bob.ToString());
